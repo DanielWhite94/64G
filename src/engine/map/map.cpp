6313999b624a424a6f1a3b3bd770fb203ede0b0d
DJW <danielwhite94@yahoo.co.uk>
2017-02-03 18:41:02 +0000

    Many changes to Map class.

--- src/engine/map/map.cpp
+++ src/engine/map/map.cpp
@@ -1,65 +1,109 @@
+#include <cassert>
+
 #include "map.h"
 #include "../graphics/renderer.h"
 
 using namespace Engine::Graphics;
 
 namespace Engine {
-	Map::Map() {
-		unsigned i, j;
-		for(i=0; i<regionsHigh; ++i)
-			for(j=0; j<regionsWide; ++j)
-				regions[i][j]=NULL;
-	}
+	namespace Map {
+		Map::Map() {
+			unsigned i, j;
+			for(i=0; i<regionsHigh; ++i)
+				for(j=0; j<regionsWide; ++j)
+					regions[i][j]=NULL;
+		}
 
-	Map::~Map() {
-		unsigned i, j;
-		for(i=0; i<regionsHigh; ++i)
-			for(j=0; j<regionsWide; ++j)
+		Map::~Map() {
+			unsigned i, j;
+			for(i=0; i<regionsHigh; ++i)
+				for(j=0; j<regionsWide; ++j)
 
-				regions[i][j]=NULL;
-	}
+					regions[i][j]=NULL;
+		}
 
-	const MapTile *Map::getTileAtCoordVec(const CoordVec &vec) const {
-		const MapRegion *region=getRegionAtCoordVec(vec);
-		if (region==NULL)
-			return NULL;
-
-		return region->getTileAtCoordVec(vec);
-	}
-
-	const MapRegion *Map::getRegionAtCoordVec(const CoordVec &vec) const {
-		CoordComponent tileX=vec.x/Physics::CoordsPerTile;
-		CoordComponent tileY=vec.y/Physics::CoordsPerTile;
-		CoordComponent regionX=tileX/MapRegion::tilesWide;
-		CoordComponent regionY=tileY/MapRegion::tilesHigh;
-
-		return regions[regionY][regionX];
-	}
-
-	MapRegion *Map::getRegionAtCoordVec(const CoordVec &vec) {
-		CoordComponent tileX=vec.x/Physics::CoordsPerTile;
-		CoordComponent tileY=vec.y/Physics::CoordsPerTile;
-		CoordComponent regionX=tileX/MapRegion::tilesWide;
-		CoordComponent regionY=tileY/MapRegion::tilesHigh;
-
-		return regions[regionY][regionX];
-	}
-
-	void Map::setTileAtCoordVec(const CoordVec &vec, const MapTile &tile) {
-		MapRegion *region=getRegionAtCoordVec(vec);
-		if (region==NULL) {
-			region=new MapRegion();
+		MapTile *Map::getTileAtCoordVec(const CoordVec &vec) {
+			MapRegion *region=getRegionAtCoordVec(vec);
 			if (region==NULL)
-				return;
+				return NULL;
+
+			return region->getTileAtCoordVec(vec);
+		}
+
+		const MapTile *Map::getTileAtCoordVec(const CoordVec &vec) const {
+			const MapRegion *region=getRegionAtCoordVec(vec);
+			if (region==NULL)
+				return NULL;
+
+			return region->getTileAtCoordVec(vec);
+		}
+
+		MapRegion *Map::getRegionAtCoordVec(const CoordVec &vec) {
+			if (vec.x<0 || vec.y<0)
+				return NULL;
 
 			CoordComponent tileX=vec.x/Physics::CoordsPerTile;
 			CoordComponent tileY=vec.y/Physics::CoordsPerTile;
 			CoordComponent regionX=tileX/MapRegion::tilesWide;
 			CoordComponent regionY=tileY/MapRegion::tilesHigh;
 
-			regions[regionY][regionX]=region;
+			if (regionX>=regionsWide || regionY>=regionsHigh)
+				return NULL;
+
+			return regions[regionY][regionX];
 		}
 
-		region->setTileAtCoordVec(vec, tile);
-	}
+		const MapRegion *Map::getRegionAtCoordVec(const CoordVec &vec) const {
+			if (vec.x<0 || vec.y<0)
+				return NULL;
+
+			CoordComponent tileX=vec.x/Physics::CoordsPerTile;
+			CoordComponent tileY=vec.y/Physics::CoordsPerTile;
+			CoordComponent regionX=tileX/MapRegion::tilesWide;
+			CoordComponent regionY=tileY/MapRegion::tilesHigh;
+
+			if (regionX>=regionsWide || regionY>=regionsHigh)
+				return NULL;
+
+			return regions[regionY][regionX];
+		}
+
+		void Map::setTileAtCoordVec(const CoordVec &vec, const MapTile &tile) {
+			MapRegion *region=getRegionAtCoordVec(vec);
+			if (region==NULL) {
+				region=new MapRegion();
+				if (region==NULL)
+					return;
+
+				CoordComponent tileX=vec.x/Physics::CoordsPerTile;
+				CoordComponent tileY=vec.y/Physics::CoordsPerTile;
+				CoordComponent regionX=tileX/MapRegion::tilesWide;
+				CoordComponent regionY=tileY/MapRegion::tilesHigh;
+
+				regions[regionY][regionX]=region;
+			}
+
+			region->setTileAtCoordVec(vec, tile);
+		}
+
+		void Map::addObject(MapObject *object) {
+			assert(object!=NULL);
+
+			// Add to object list.
+			objects.push_back(object);
+
+			// Update tiles.
+			CoordVec vec;
+			CoordVec vec1=object->getCoordTopLeft();
+			CoordVec vec2=object->getCoordBottomRight();
+			vec1.x=floor(vec1.x/Physics::CoordsPerTile)*Physics::CoordsPerTile;
+			vec1.y=floor(vec1.y/Physics::CoordsPerTile)*Physics::CoordsPerTile;
+			vec2.x=floor(vec2.x/Physics::CoordsPerTile)*Physics::CoordsPerTile;
+			vec2.y=floor(vec2.y/Physics::CoordsPerTile)*Physics::CoordsPerTile;
+
+			for(vec.y=vec1.y; vec.y<=vec2.y; vec.y+=Physics::CoordsPerTile)
+				for(vec.x=vec1.x; vec.x<=vec2.x; vec.x+=Physics::CoordsPerTile)
+					getTileAtCoordVec(vec)->addObject(object);
+		}
+	};
 };
--- src/engine/map/map.h
+++ src/engine/map/map.h
@@ -1,29 +1,41 @@
 #ifndef ENGINE_GRAPHICS_MAP_H
 #define ENGINE_GRAPHICS_MAP_H
 
+#include <vector>
+
+#include "mapobject.h"
 #include "mapregion.h"
 #include "maptile.h"
 #include "../physics/coord.h"
 
+using namespace std;
+
 using namespace Engine;
+using namespace Engine::Map;
 using namespace Engine::Physics;
 
 namespace Engine {
-	class Map {
-	public:
-		static const unsigned regionsWide=256, regionsHigh=256;
+	namespace Map {
+		class Map {
+		public:
+			static const unsigned regionsWide=256, regionsHigh=256;
 
-		Map();
-		~Map();
+			Map();
+			~Map();
 
-		const MapTile *getTileAtCoordVec(const CoordVec &vec) const;
-		const MapRegion *getRegionAtCoordVec(const CoordVec &vec) const;
-		MapRegion *getRegionAtCoordVec(const CoordVec &vec);
+			MapTile *getTileAtCoordVec(const CoordVec &vec);
+			const MapTile *getTileAtCoordVec(const CoordVec &vec) const;
+			MapRegion *getRegionAtCoordVec(const CoordVec &vec);
+			const MapRegion *getRegionAtCoordVec(const CoordVec &vec) const;
 
-		void setTileAtCoordVec(const CoordVec &vec, const MapTile &tile);
+			void setTileAtCoordVec(const CoordVec &vec, const MapTile &tile);
 
-	private:
-		MapRegion *regions[regionsHigh][regionsWide];
+			void addObject(MapObject *object);
+		private:
+			MapRegion *regions[regionsHigh][regionsWide];
+
+			vector<MapObject *> objects;
+		};
 	};
 };
 
