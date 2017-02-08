2b55e50ccfe4924da56c4601a2529db35fe27d96
DJW <danielwhite94@yahoo.co.uk>
2017-02-08 23:05:32 +0000

    Added HitMask setColumn and setRow.

--- src/engine/physics/hitmask.cpp
+++ src/engine/physics/hitmask.cpp
@@ -22,6 +22,30 @@ namespace Engine {
 			return (bitset>>shift)&1;
 		}
 
+		void HitMask::setColumn(int x, bool value) {
+			if (x<0 || x>=8)
+				return;
+
+			HitMask column0=HitMask(column0Mask);
+			column0.translateRight(x);
+			if (value)
+				bitset|=column0.bitset;
+			else
+				bitset&=~column0.bitset;
+		}
+
+		void HitMask::setRow(int y, bool value) {
+			if (y<0 || y>=8)
+				return;
+
+			HitMask row0=HitMask(row0Mask);
+			row0.translateDown(y);
+			if (value)
+				bitset|=row0.bitset;
+			else
+				bitset&=~row0.bitset;
+		}
+
 		void HitMask::setXY(unsigned x, unsigned y, bool value) {
 			unsigned shift=x+y*8;
 			bitset&=~(((uint64_t)1)<<shift);
--- src/engine/physics/hitmask.h
+++ src/engine/physics/hitmask.h
@@ -13,6 +13,8 @@ namespace Engine {
 
 			bool getXY(unsigned x, unsigned y) const; // 0<=x,y<8
 
+			void setColumn(int x, bool value);
+			void setRow(int y, bool value);
 			void setXY(unsigned x, unsigned y, bool value); // 0<=x,y<8
 
 			HitMask &operator|=(const HitMask &rhs) {
@@ -25,8 +27,10 @@ namespace Engine {
 				return lhs;
 			}
 		private:
-			uint64_t bitset;
+			static const uint64_t row0Mask=0x00000000000000FFllu;
+			static const uint64_t column0Mask=0x0101010101010101llu;
 
+			uint64_t bitset;
 		};
 	};
 };
