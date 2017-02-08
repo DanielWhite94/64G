801b16ab71dc16973a6567f435fffa8b64da662a
DJW <danielwhite94@yahoo.co.uk>
2017-02-08 23:01:43 +0000

    Added HitMask OR operators.

--- src/engine/physics/hitmask.h
+++ src/engine/physics/hitmask.h
@@ -14,6 +14,16 @@ namespace Engine {
 			bool getXY(unsigned x, unsigned y) const; // 0<=x,y<8
 
 			void setXY(unsigned x, unsigned y, bool value); // 0<=x,y<8
+
+			HitMask &operator|=(const HitMask &rhs) {
+				this->bitset|=rhs.bitset;
+				return *this;
+			}
+
+			friend HitMask operator|(HitMask lhs, const HitMask &rhs) {
+				lhs|=rhs;
+				return lhs;
+			}
 		private:
 			uint64_t bitset;
 
