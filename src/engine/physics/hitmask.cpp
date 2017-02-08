df2df5a48619ffce5926d3883fa8d85b9e832d74
DJW <danielwhite94@yahoo.co.uk>
2017-02-08 23:06:12 +0000

    Refactored HitMask setXY slightly.

--- src/engine/physics/hitmask.cpp
+++ src/engine/physics/hitmask.cpp
@@ -47,7 +47,7 @@ namespace Engine {
 		}
 
 		void HitMask::setXY(unsigned x, unsigned y, bool value) {
-			unsigned shift=x+y*8;
+			const unsigned shift=x+y*8;
 			bitset&=~(((uint64_t)1)<<shift);
 			bitset|=(((uint64_t)value)<<shift);
 		}
