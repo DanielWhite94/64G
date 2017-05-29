909aeb8a4ea581166ba7045a1795b8d4b7f5dfb8
DJW <danielwhite94@yahoo.co.uk>
2017-05-29 16:46:06 +0100

    Added Util::angleFromXYToXY.

--- src/engine/util.cpp
+++ src/engine/util.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cmath>
 
 #include "util.h"
 
@@ -11,4 +12,8 @@ int Util::floordiv(int n, int d) {
 		ldiv_t res=ldiv(n,d);
 		return res.rem ? res.quot-1 : res.quot;
 	}
+}
+
+double Util::angleFromXYToXY(double x1, double y1, double x2, double y2) {
+	return atan2(-(y2-y1), (x2-x1));
 }
--- src/engine/util.h
+++ src/engine/util.h
@@ -5,6 +5,8 @@ namespace Engine {
 	class Util {
 	public:
 		static int floordiv(int n, int d);
+
+		static double angleFromXYToXY(double x1, double y1, double x2, double y2);
 	};
 };
 
