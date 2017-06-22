96815e840110c7d4c5c8d9f56c86c1b1f051a39e
DJW <danielwhite94@yahoo.co.uk>
2017-06-22 22:20:41 +0100

    demogen.cpp now adds a few houses.

--- src/demogen/demogen.cpp
+++ src/demogen/demogen.cpp
@@ -80,6 +80,12 @@ int main(int argc, char **argv) {
 	MapGen::addBuiltinObjectForestWithTestFunctor(map, MapGen::BuiltinObject::Tree1, CoordVec(0*Physics::CoordsPerTile, 0*Physics::CoordsPerTile), CoordVec(width*Physics::CoordsPerTile, height*Physics::CoordsPerTile), CoordVec(3*Physics::CoordsPerTile, 3*Physics::CoordsPerTile), &demogenForestTestFunctorIsLand, NULL);
 	*/
 
+	// Add a test house.
+	printf("Adding houses...\n");
+	MapGen::addHouse(map, 950, 730, 10, 8, 4);
+	MapGen::addHouse(map, 963, 729, 6, 6, 4);
+	MapGen::addHouse(map, 965, 737, 8, 7, 4);
+
 	// Save map.
 	if (!map->save()) {
 		printf("Could not save map to '%s'.\n", outputPath);
