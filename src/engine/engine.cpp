25043be7d56c5b9a7c9e13ada1d90dcf3f067bb1
DJW <danielwhite94@yahoo.co.uk>
2017-06-22 22:20:18 +0100

    Updated engine's starting player position.

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv) {
 	}
 
 	// Add player object.
-	MapObject objectPlayer(CoordAngle0, CoordVec(205*Physics::CoordsPerTile, 521*Physics::CoordsPerTile), 1, 1);
+	MapObject objectPlayer(CoordAngle0, CoordVec(959*Physics::CoordsPerTile, 725*Physics::CoordsPerTile), 1, 1);
 	HitMask playerHitmask;
 	const unsigned playerW=4, playerH=6;
 	unsigned playerX, playerY;
