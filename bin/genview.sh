da6ec7626e1be403338cbb51a60ac20fbecbc38d
DJW <danielwhite94@yahoo.co.uk>
2017-10-10 21:57:45 +0100

    Reset genview.sh to normal viewpoint.

--- bin/genview.sh
+++ bin/genview.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ./demogen $1 $2 $3
-#./mappng $3 0 0 $1 $2 1024 1024 output.png
-./mappng $3 700 440 300 300 1024 1024 output.png
+./mappng $3 0 0 $1 $2 1024 1024 output.png
 eog output.png
