e656a66cf2c07d7a404432963e381d56f63e9b4f
DJW <danielwhite94@yahoo.co.uk>
2017-10-21 20:44:19 +0100

    mappng now loops over region-by-region so that each region is only loaded in once. Also improved output.

--- src/mappng/mappng.cpp
+++ src/mappng/mappng.cpp
@@ -1,12 +1,14 @@
 #include <cassert>
+#include <cmath>
 #include <cstdbool>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
 #include <png.h>
 
 #include "../engine/map/map.h"
 #include "../engine/map/mapgen.h"
 #include "../engine/map/mapobject.h"
+#include "../engine/util.h"
 
 using namespace Engine;
 using namespace Engine::Map;
@@ -67,98 +69,139 @@ int main(int argc, char **argv) {
 	png_set_IHDR(pngPtr, infoPtr, imageWidth, imageHeight, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 	png_write_info(pngPtr, infoPtr);
 
-	// Write pixels.
-	printf("Writing pixel data...\n");
+	// Create rows.
+	png_bytep pngRows=(png_bytep)malloc(imageHeight*imageWidth*3*sizeof(png_byte));
 
 	double xScale=((double)mapTileWidth)/((double)imageWidth);
 	double yScale=((double)mapTileHeight)/((double)imageHeight);
 
-	unsigned x, y;
-	png_bytep pngRow=(png_bytep)malloc(3*imageWidth*sizeof(png_byte));
-	for(y=0;y<imageHeight;++y) {
-		for(x=0;x<imageWidth;++x) {
-			// Find tile represented by this pixel.
-			// TODO: It would be better to scale properly (i.e consider tiles in this neighbourhood).
-			CoordComponent tileX=x*xScale+mapTileX;
-			CoordComponent tileY=y*yScale+mapTileY;
+	const unsigned tileX0=mapTileX;
+	const unsigned tileY0=mapTileY;
+	const unsigned tileX1=(imageWidth-1)*xScale+mapTileX;
+	const unsigned tileY1=(imageHeight-1)*yScale+mapTileY;
+	const unsigned regionX0=tileX0/MapRegion::tilesWide;
+	const unsigned regionY0=tileY0/MapRegion::tilesHigh;
+	const unsigned regionX1=tileX1/MapRegion::tilesWide+1;
+	const unsigned regionY1=tileY1/MapRegion::tilesHigh+1;
 
-			CoordVec vec={tileX*Physics::CoordsPerTile, tileY*Physics::CoordsPerTile};
-			const MapTile *tile=map->getTileAtCoordVec(vec);
-			assert(tile!=NULL); // TODO: Handle better
+	unsigned regionX, regionY;
+	for(regionY=regionY0; regionY<regionY1; ++regionY) {
+		const unsigned regionTileY0=regionY*MapRegion::tilesHigh;
+		const unsigned regionTileY1=regionTileY0+MapRegion::tilesHigh;
+		for(regionX=regionX0; regionX<regionX1; ++regionX) {
+			const unsigned regionTileX0=regionX*MapRegion::tilesWide;
+			const unsigned regionTileX1=regionTileX0+MapRegion::tilesWide;
 
-			// Choose colour (based on topmost layer with a texture set).
-			uint8_t r=0, g=0, b=0;
+			const unsigned imageX0=std::max((unsigned)0, (unsigned)floor((regionTileX0-mapTileX)/xScale));
+			const unsigned imageX1=std::min((unsigned)imageWidth, (unsigned)floor((regionTileX1-mapTileX)/xScale));
+			const unsigned imageY0=std::max((unsigned)0, (unsigned)floor((regionTileY0-mapTileY)/yScale));
+			const unsigned imageY1=std::min((unsigned)imageHeight, (unsigned)floor((regionTileY1-mapTileY)/yScale));
 
-			for(int z=MapTile::layersMax-1; z>=0; --z) {
-				const MapTile::Layer *layer=tile->getLayer(z);
-				switch(layer->textureId) {
-					case MapGen::TextureIdNone:
-						continue; // Try next layer instead
-					break;
-					case MapGen::TextureIdGrass0:
-						r=0,g=255,b=0;
-					break;
-					case MapGen::TextureIdGrass1:
-					case MapGen::TextureIdGrass2:
-					case MapGen::TextureIdGrass3:
-					case MapGen::TextureIdGrass4:
-						r=0,g=200,b=0;
-					break;
-					case MapGen::TextureIdGrass5:
-						r=0,g=150,b=0;
-					break;
-					case MapGen::TextureIdBrickPath:
-					case MapGen::TextureIdDock:
-						r=102,g=51,b=0;
-					break;
-					case MapGen::TextureIdDirt:
-						r=0xCC,g=0x66,b=0x00;
-					break;
-					case MapGen::TextureIdWater:
-						r=0,g=0,b=255;
-					break;
-					case MapGen::TextureIdTree1:
-					case MapGen::TextureIdTree2:
-						r=0,g=100,b=0;
-					break;
-					case MapGen::TextureIdMan1:
-					case MapGen::TextureIdOldManN:
-					case MapGen::TextureIdOldManE:
-					case MapGen::TextureIdOldManS:
-					case MapGen::TextureIdOldManW:
-						r=255,g=0,b=0;
-					break;
-					case MapGen::TextureIdHouseDoorBL:
-					case MapGen::TextureIdHouseDoorBR:
-					case MapGen::TextureIdHouseDoorTL:
-					case MapGen::TextureIdHouseDoorTR:
-					case MapGen::TextureIdHouseRoof:
-					case MapGen::TextureIdHouseRoofTop:
-					case MapGen::TextureIdHouseWall2:
-					case MapGen::TextureIdHouseWall3:
-					case MapGen::TextureIdHouseWall4:
-					case MapGen::TextureIdHouseChimney:
-					case MapGen::TextureIdHouseChimneyTop:
-						r=255,g=128,b=0;
-					break;
-					case MapGen::TextureIdNB:
-						assert(false);
-						continue; // Try next layer instead
-					break;
+			unsigned imageX, imageY;
+			for(imageY=imageY0; imageY<imageY1; ++imageY) {
+				for(imageX=imageX0; imageX<imageX1; ++imageX) {
+					// Find tile represented by this pixel.
+					// TODO: It would be better to scale properly (i.e consider tiles in this neighbourhood).
+					CoordComponent imageTileX=imageX*xScale+mapTileX;
+					CoordComponent imageTileY=imageY*yScale+mapTileY;
+
+					CoordVec vec={imageTileX*Physics::CoordsPerTile, imageTileY*Physics::CoordsPerTile};
+					const MapTile *tile=map->getTileAtCoordVec(vec);
+					assert(tile!=NULL); // TODO: Handle better
+
+					// Choose colour (based on topmost layer with a texture set).
+					uint8_t r=0, g=0, b=0;
+
+					for(int z=MapTile::layersMax-1; z>=0; --z) {
+						const MapTile::Layer *layer=tile->getLayer(z);
+						switch(layer->textureId) {
+							case MapGen::TextureIdNone:
+								continue; // Try next layer instead
+							break;
+							case MapGen::TextureIdGrass0:
+								r=0,g=255,b=0;
+							break;
+							case MapGen::TextureIdGrass1:
+							case MapGen::TextureIdGrass2:
+							case MapGen::TextureIdGrass3:
+							case MapGen::TextureIdGrass4:
+								r=0,g=200,b=0;
+							break;
+							case MapGen::TextureIdGrass5:
+								r=0,g=150,b=0;
+							break;
+							case MapGen::TextureIdBrickPath:
+							case MapGen::TextureIdDock:
+								r=102,g=51,b=0;
+							break;
+							case MapGen::TextureIdDirt:
+								r=0xCC,g=0x66,b=0x00;
+							break;
+							case MapGen::TextureIdWater:
+								r=0,g=0,b=255;
+							break;
+							case MapGen::TextureIdTree1:
+							case MapGen::TextureIdTree2:
+								r=0,g=100,b=0;
+							break;
+							case MapGen::TextureIdMan1:
+							case MapGen::TextureIdOldManN:
+							case MapGen::TextureIdOldManE:
+							case MapGen::TextureIdOldManS:
+							case MapGen::TextureIdOldManW:
+								r=255,g=0,b=0;
+							break;
+							case MapGen::TextureIdHouseDoorBL:
+							case MapGen::TextureIdHouseDoorBR:
+							case MapGen::TextureIdHouseDoorTL:
+							case MapGen::TextureIdHouseDoorTR:
+							case MapGen::TextureIdHouseRoof:
+							case MapGen::TextureIdHouseRoofTop:
+							case MapGen::TextureIdHouseWall2:
+							case MapGen::TextureIdHouseWall3:
+							case MapGen::TextureIdHouseWall4:
+							case MapGen::TextureIdHouseChimney:
+							case MapGen::TextureIdHouseChimneyTop:
+								r=255,g=128,b=0;
+							break;
+							case MapGen::TextureIdNB:
+								assert(false);
+								continue; // Try next layer instead
+							break;
+						}
+
+						break; // We have found something to draw
+					}
+
+					// Write pixel.
+					pngRows[(imageY*imageWidth+imageX)*3+0]=r;
+					pngRows[(imageY*imageWidth+imageX)*3+1]=g;
+					pngRows[(imageY*imageWidth+imageX)*3+2]=b;
 				}
-
-				break; // We have found something to draw
 			}
 
-			// Write pixel.
-			pngRow[x*3+0]=r;
-			pngRow[x*3+1]=g;
-			pngRow[x*3+2]=b;
+			// Update progress.
+			Util::clearConsoleLine();
+			double progress=100.0*(regionX+regionY*(regionX1-regionX0))/((regionY1-regionY0)*(regionX1-regionX0));
+			printf("Creating rows... %.1f%%", progress);
+			fflush(stdout);
 		}
-		png_write_row(pngPtr, pngRow);
 	}
+	printf("\n");
 
-	free(pngRow);
+	// Write pixels.
+	int pngY;
+	for(pngY=0; pngY<imageHeight; ++pngY) {
+		png_write_row(pngPtr, &pngRows[(pngY*imageWidth+0)*3*sizeof(png_byte)]);
+
+		Util::clearConsoleLine();
+		double progress=100.0*pngY/imageHeight;
+		printf("Writing pixel data... %.1f%%", progress);
+		fflush(stdout);
+	}
+	printf("\n");
+
+	free(pngRows);
 
 	// Finish writing image file.
 	png_write_end(pngPtr, NULL);
