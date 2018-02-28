ac65b8532a3b83bfcb971ceae1872488f646e25a
DJW <dan@fake.com>
2018-02-28 10:40:59 +0000

    Further tweaked FbnNoise::eval.

--- src/engine/fbnnoise.cpp
+++ src/engine/fbnnoise.cpp
@@ -20,9 +20,9 @@ namespace Engine {
 		// Loop in reverse so that we start with amp small, so that we do not lose as much precision when summing result.
 		for(int i=octaves-1;i>=0;--i) {
 			// TODO: Is calculating these each time needed? (or can we just do e.g. amp*=0.5 each time)
-			double amp=pow(0.5, i);
-			double freq=frequency*pow(2.0, i);
-			result+=amp*baseNoise->eval(x*freq, y*freq);
+			double exp2I=exp2(i);
+			double freq=frequency*exp2I;
+			result+=baseNoise->eval(x*freq, y*freq)/exp2I;
 		}
 
 		double dividend=2.0-pow(0.5, octaves-1);
