5fe4dc27a5ab38628e26e4ca07f7fa7156161b3f
DJW <dan@fake.com>
2019-01-30 19:10:25 +0000

    Removed unused map field from mapeditor main class

--- src/mapeditor/main.h
+++ src/mapeditor/main.h
@@ -1,8 +1,6 @@
 #ifndef MAPEDITOR_MAIN_H
 #define MAPEDITOR_MAIN_H
 
-#include "../engine/map/map.h"
-
 #include "mainwindow.h"
 
 namespace MapEditor {
@@ -12,7 +10,6 @@ namespace MapEditor {
 		~Main();
 
 	private:
-		class Map *map;
 		MainWindow *mainWindow;
 	};
 };
