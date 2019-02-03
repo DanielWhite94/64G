75ca0c7f9e08e1e343b5c92a19e6826cdb75d73e
DJW <dan@fake.com>
2019-02-03 13:52:03 +0000

    Began writing maptiled 'web tile map' module

---
+++ src/engine/map/maptiled.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+#include "maptiled.h"
+#include "../physics/coord.h"
+#include "../util.h"
+
+using namespace Engine;
+
+namespace Engine {
+	namespace Map {
+		MapTiled::MapTiled() {
+		}
+
+		MapTiled::~MapTiled() {
+		};
+
+	};
+};
---
+++ src/engine/map/maptiled.h
@@ -0,0 +1,20 @@
+#ifndef ENGINE_MAP_MAPTILED_H
+#define ENGINE_MAP_MAPTILED_H
+
+#include "map.h"
+#include "maptexture.h"
+#include "maptile.h"
+#include "../util.h"
+
+namespace Engine {
+	namespace Map {
+		class MapTiled {
+		public:
+			MapTiled();
+			~MapTiled();
+		private:
+		};
+	};
+};
+
+#endif
