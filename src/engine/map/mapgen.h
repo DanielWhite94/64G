a570befb1f953eab8ed47c92f4146bec678b3c3b
DJW <asd@example.com>
2020-08-10 23:59:50 +0100

    Removed pointless TODO

--- src/engine/map/mapgen.h
+++ src/engine/map/mapgen.h
@@ -221,7 +221,6 @@ namespace Engine {
 			static void modifyTilesMany(class Map *map, unsigned x, unsigned y, unsigned width, unsigned height, size_t functorArrayCount, MapGen::ModifyTilesManyEntry functorArray[], ModifyTilesProgress *progressFunctor, void *progressUserData);
 
 			static double narySearch(class Map *map, unsigned x, unsigned y, unsigned width, unsigned height, int n, double threshold, double epsilon, double sampleMin, double sampleMax, NArySearchGetFunctor *getFunctor, void *getUserData); // Returns (approximate) height/moisture etc value for which threshold fraction of the tiles in the given region are lower than said value.
-			// TODO: Make narySearch map argument const. Currently restricted by dependency on modifyTiles
 			static int narySearchValueToSample(const NArySearchData *data, double value);
 			static double narySearchSampleToValue(const NArySearchData *data, int sample);
 
