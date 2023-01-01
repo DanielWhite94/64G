9a0955ced5924624a10b710776efc3fd56680427
Daniel White <fake@example.com>
2023-01-01 23:03:37 +0000

    Added Gen::searchMany which is to search as modifyTilesMany is to modifyTiles

--- src/engine/gen/search.cpp
+++ src/engine/gen/search.cpp
@@ -9,115 +9,187 @@ using namespace Engine;
 
 namespace Engine {
 	namespace Gen {
+		struct SearchData {
+			SearchDataEntry *entries;
+			size_t count;
+		};
+
 		double search(class Map *map, unsigned x, unsigned y, unsigned width, unsigned height, unsigned threadCount, int n, double threshold, double epsilon, double sampleMin, double sampleMax, SearchGetFunctor *getFunctor, void *getUserData) {
 			assert(map!=NULL);
 			assert(n>0);
 			assert(0.0<=threshold<=1.0);
 			assert(getFunctor!=NULL);
 
-			// Calculate expected iterMax and use it just in case we cannot narrow down the range for some reason.
-			// This calculation is based on the idea that each iteration reduces the range by a factpr of #samples+1.
-			int iterMax=ceil(log((sampleMax-sampleMin)/(2*epsilon))/log(n+1));
+			SearchManyEntry searchManyEntry;
+			searchManyEntry.threshold=threshold;
+			searchManyEntry.epsilon=epsilon;
+			searchManyEntry.sampleMin=sampleMin;
+			searchManyEntry.sampleMax=sampleMax;
+			searchManyEntry.sampleCount=n;
+			searchManyEntry.getFunctor=getFunctor;
+			searchManyEntry.getUserData=getUserData;
+
+			searchMany(map, x, y, width, height, threadCount, 1, &searchManyEntry);
+
+			return searchManyEntry.result;
+		}
+
+		void searchMany(class Map *map, unsigned x, unsigned y, unsigned width, unsigned height, unsigned threadCount, size_t entryArrayCount, SearchManyEntry entryArray[]) {
+			assert(map!=NULL);
 
 			// Initialize data struct.
 			SearchData data;
-			data.map=map;
-			data.getFunctor=getFunctor;
-			data.getUserData=getUserData;
-			data.sampleCount=n;
-			data.sampleTally=(unsigned long long int *)malloc(sizeof(unsigned long long int)*data.sampleCount); // TODO: Check return.
-			data.sampleMin=sampleMin;
-			data.sampleMax=sampleMax;
+			data.count=entryArrayCount;
+			data.entries=(SearchDataEntry *)malloc(sizeof(SearchDataEntry)*entryArrayCount); // TODO: Check return
 
-			// Loop, running iterations up to the maximum.
-			for(int iter=0; iter<iterMax; ++iter) {
-				assert(data.sampleMax>=data.sampleMin);
+			int trueIterMax=0;
+			for(size_t i=0; i<data.count; ++i) {
+				SearchDataEntry *entry=&data.entries[i];
 
-				// Update cached values
-				data.sampleRange=data.sampleMax-data.sampleMin;
-				data.sampleConversionFactor=(data.sampleCount+1.0)/data.sampleRange;
+				entry->map=map;
+				entry->getFunctor=entryArray[i].getFunctor;
+				entry->getUserData=entryArray[i].getUserData;
+				entry->sampleCount=entryArray[i].sampleCount;
+				entry->sampleTally=(unsigned long long int *)malloc(sizeof(unsigned long long int)*entry->sampleCount); // TODO: Check return.
+				entry->sampleMin=entryArray[i].sampleMin;
+				entry->sampleMax=entryArray[i].sampleMax;
+				entry->threshold=entryArray[i].threshold;
+				entry->epsilon=entryArray[i].epsilon;
 
-				// Have we hit desired accuracy?
-				if (data.sampleRange/2.0<=epsilon)
-					break;
+				// Calculate expected iterMax and use it just in case we cannot narrow down the range for some reason.
+				// This calculation is based on the idea that each iteration reduces the range by a factpr of #samples+1.
+				entry->iterMax=ceil(log((entry->sampleMax-entry->sampleMin)/(2*entry->epsilon))/log(entry->sampleCount+1));
 
-				// Clear data struct for this iteration.
-				for(int i=0; i<data.sampleCount; ++i)
-					data.sampleTally[i]=0;
-				data.sampleTotal=0;
+				if (entry->iterMax>trueIterMax)
+					trueIterMax=entry->iterMax;
+			}
+
+			// Loop, running iterations up to the maximum of the maximums.
+			for(int iter=0; iter<trueIterMax; ++iter) {
+				// Prepare each operation
+				for(size_t i=0; i<data.count; ++i) {
+					SearchDataEntry *entry=&data.entries[i];
+
+					assert(entry->sampleMax>=entry->sampleMin);
+
+					// Update cached values
+					entry->sampleRange=entry->sampleMax-entry->sampleMin;
+					entry->sampleConversionFactor=(entry->sampleCount+1.0)/entry->sampleRange;
+
+					// Break out early if no need to compute another iteration for this operation
+					if (entry->sampleRange/2.0<=entry->epsilon)
+						continue;
+
+					// Clear data struct for this iteration.
+					for(int j=0; j<entry->sampleCount; ++j)
+						entry->sampleTally[j]=0;
+					entry->sampleTotal=0;
+				}
+
+				// Output
+				printf("	Iteration %i/%i:\n", iter+1, trueIterMax);
+				for(size_t i=0; i<data.count; ++i) {
+					SearchDataEntry *entry=&data.entries[i];
+					if (entry->sampleRange/2.0<=entry->epsilon)
+						printf("		%2llu: %f\n", (unsigned long long)i, (entry->sampleMin+entry->sampleMax)/2.0); // already finished
+					else
+						printf("		%2llu: [%f, %f] (range %f, epsilon %f)\n", (unsigned long long)i, entry->sampleMin, entry->sampleMax, entry->sampleRange, entry->epsilon);
+				}
 
 				// Run data collection functor.
 				char progressString[1024];
-				sprintf(progressString, "	%i/%i - interval [%f, %f] (range %f): ", iter+1, iterMax, data.sampleMin, data.sampleMax, data.sampleRange);
-				Gen::modifyTiles(data.map, x, y, width, height, threadCount, &searchModifyTilesFunctor, &data, &Gen::modifyTilesProgressString, (void *)progressString);
+				sprintf(progressString, "		calculating: ");
+				Gen::modifyTiles(map, x, y, width, height, threadCount, &searchManyModifyTilesFunctor, &data, &Gen::modifyTilesProgressString, (void *)progressString);
 				printf("\n");
 
 				// Update min/max based on collected data.
 				// TODO: this can be improved by looping to find window which contains the fraction we want, then updating min/max together and breaking
-				double newSampleMin=data.sampleMin;
-				double newSampleMax=data.sampleMax;
-				int sampleIndex;
-				unsigned long long int sampleCumulative=0;
-				for(sampleIndex=data.sampleCount-1; sampleIndex>=0; --sampleIndex) {
-					sampleCumulative+=data.sampleTally[sampleIndex];
-					double fraction=sampleCumulative/((double)data.sampleTotal);
-					double sampleValue=searchSampleToValue(&data, sampleIndex);
-					if (fraction>threshold)
-						newSampleMin=std::max(newSampleMin, sampleValue);
-					if (fraction<threshold)
-						newSampleMax=std::min(newSampleMax, sampleValue);
+				for(size_t i=0; i<data.count; ++i) {
+					SearchDataEntry *entry=&data.entries[i];
+
+					if (entry->sampleRange/2.0<=entry->epsilon)
+						continue;
+
+					double newSampleMin=entry->sampleMin;
+					double newSampleMax=entry->sampleMax;
+					int sampleIndex;
+					unsigned long long int sampleCumulative=0;
+					for(sampleIndex=entry->sampleCount-1; sampleIndex>=0; --sampleIndex) {
+						sampleCumulative+=entry->sampleTally[sampleIndex];
+						double fraction=sampleCumulative/((double)entry->sampleTotal);
+						double sampleValue=searchSampleToValue(entry, sampleIndex);
+						if (fraction>entry->threshold)
+							newSampleMin=std::max(newSampleMin, sampleValue);
+						if (fraction<entry->threshold)
+							newSampleMax=std::min(newSampleMax, sampleValue);
+					}
+					entry->sampleMin=newSampleMin;
+					entry->sampleMax=newSampleMax;
 				}
-				data.sampleMin=newSampleMin;
-				data.sampleMax=newSampleMax;
 			}
 
 			// Tidy up.
-			free(data.sampleTally);
-
-			// Write out final range.
-			printf("	final interval [%f, %f] (range %f, 2*epsilon %f)\n", data.sampleMin, data.sampleMax, data.sampleMax-data.sampleMin, 2*epsilon);
+			for(size_t i=0; i<data.count; ++i)
+				free(data.entries[i].sampleTally);
+			free(data.entries);
 
 			// Return midpoint of interval.
-			return (data.sampleMin+data.sampleMax)/2.0;
+			printf("	Final intervals:\n");
+			for(size_t i=0; i<data.count; ++i) {
+				SearchDataEntry *entry=&data.entries[i];
+				entryArray[i].result=(entry->sampleMin+entry->sampleMax)/2.0;
+
+				// Write out final range.
+				printf("		%2llu: %f [%f, %f] (range %f, 2*epsilon %f)\n", (unsigned long long)i, entryArray[i].result, entry->sampleMin, entry->sampleMax, entry->sampleMax-entry->sampleMin, 2*entry->epsilon);
+			}
 		}
 
-		int searchValueToSample(const SearchData *data, double value) {
-			assert(data!=NULL);
+		int searchValueToSample(const SearchDataEntry *entry, double value) {
+			assert(entry!=NULL);
 
 			// Determine which sample bucket this value falls into.
-			int result=floor((value-data->sampleMin)*data->sampleConversionFactor)-1;
+			int result=floor((value-entry->sampleMin)*entry->sampleConversionFactor)-1;
 
 			if (result<0)
 				result=0;
-			if (result>=data->sampleCount)
-				result=data->sampleCount-1;
+			if (result>=entry->sampleCount)
+				result=entry->sampleCount-1;
 
 			return result;
 		}
 
-		double searchSampleToValue(const SearchData *data, int sample) {
-			assert(data!=NULL);
-			assert(sample>=0 && sample<data->sampleCount);
+		double searchSampleToValue(const SearchDataEntry *entry, int sample) {
+			assert(entry!=NULL);
+			assert(sample>=0 && sample<entry->sampleCount);
 
-			return data->sampleMin+(sample+1.0)/data->sampleConversionFactor;
+			return entry->sampleMin+(sample+1.0)/entry->sampleConversionFactor;
 		}
 
-		void searchModifyTilesFunctor(unsigned threadId, class Map *map, unsigned x, unsigned y, void *userData) {
+		void searchManyModifyTilesFunctor(unsigned threadId, class Map *map, unsigned x, unsigned y, void *userData) {
 			assert(map!=NULL);
 			assert(userData!=NULL);
 
 			SearchData *data=(SearchData *)userData;
 
-			// Grab value.
-			double value=data->getFunctor(map, x, y, data->getUserData);
+			// Loop over all operations we need to perform
+			for(size_t i=0; i<data->count; ++i) {
+				SearchDataEntry *entry=&data->entries[i];
 
-			// Compute sample index.
-			int sample=searchValueToSample(data, value);
+				// Have we hit desired accuracy for this operation?
+				if (entry->sampleRange/2.0<=entry->epsilon)
+					continue;
 
-			// Update tally array and total.
-			// TODO: Fix this to make the increments thread safe (make the struct members atomic presumably, or otherwise split the counters for each thread and combine at the end)
-			++data->sampleTally[sample];
-			++data->sampleTotal;
+				// Grab value.
+				double value=entry->getFunctor(map, x, y, entry->getUserData);
+
+				// Compute sample index.
+				int sample=searchValueToSample(entry, value);
+
+				// Update tally array and total.
+				// TODO: Fix this to make the increments thread safe (make the struct members atomic presumably, or otherwise split the counters for each thread and combine at the end)
+				++entry->sampleTally[sample];
+				++entry->sampleTotal;
+			}
 		}
 
 		double searchGetFunctorHeight(class Map *map, unsigned x, unsigned y, void *userData) {
--- src/engine/gen/search.h
+++ src/engine/gen/search.h
@@ -5,7 +5,8 @@
 
 namespace Engine {
 	namespace Gen {
-		void searchModifyTilesFunctor(unsigned threadId, class Map *map, unsigned x, unsigned y, void *userData);
+		void searchManyModifyTilesFunctor(unsigned threadId, class Map *map, unsigned x, unsigned y, void *userData);
+
 		double searchGetFunctorHeight(class Map *map, unsigned x, unsigned y, void *userData);
 		double searchGetFunctorTemperature(class Map *map, unsigned x, unsigned y, void *userData);
 		double searchGetFunctorMoisture(class Map *map, unsigned x, unsigned y, void *userData);
@@ -13,7 +14,7 @@ namespace Engine {
 
 		typedef double (SearchGetFunctor)(class Map *map, unsigned x, unsigned y, void *userData);
 
-		struct SearchData {
+		struct SearchDataEntry {
 			class Map *map;
 
 			SearchGetFunctor *getFunctor;
@@ -23,11 +24,32 @@ namespace Engine {
 			double sampleConversionFactor; // equal to (data->sampleCount+1.0)/data->sampleRange, see MapGen::searchValueToSample
 			int sampleCount;
 			unsigned long long int *sampleTally, sampleTotal;
+
+			int iterMax;
+
+			double threshold;
+			double epsilon;
+		};
+
+		struct SearchManyEntry {
+			// threshold to aim for and allowed error
+			double threshold;
+			double epsilon;
+
+			double sampleMin, sampleMax; // precomputed min/max values that can be encountered
+			int sampleCount; // how many samples to take per iteration (1 gives binary search, 2 ternary etc)
+
+			SearchGetFunctor *getFunctor;
+			void *getUserData;
+
+			double result;  // result written back into here
 		};
 
 		double search(class Map *map, unsigned x, unsigned y, unsigned width, unsigned height, unsigned threadCount, int n, double threshold, double epsilon, double sampleMin, double sampleMax, SearchGetFunctor *getFunctor, void *getUserData); // Returns (approximate) height/moisture etc value for which threshold fraction of the tiles in the given region are lower than said value.
-		int searchValueToSample(const SearchData *data, double value);
-		double searchSampleToValue(const SearchData *data, int sample);
+		void searchMany(class Map *map, unsigned x, unsigned y, unsigned width, unsigned height, unsigned threadCount, size_t entryArrayCount, SearchManyEntry entryArray[]); // Same as running several search operations
+
+		int searchValueToSample(const SearchDataEntry *entry, double value);
+		double searchSampleToValue(const SearchDataEntry *entry, int sample);
 	};
 };
 
