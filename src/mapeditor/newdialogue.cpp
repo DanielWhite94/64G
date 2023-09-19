e10711bf419fc5b83de7fd7cc9d8ca57ccafe991
DJW <fake@example.com>
2023-09-19 20:04:58 +0100

    Upgraded the map editor's New dialogue to allow specifying size in different units and giving a disk space estimation.

--- src/mapeditor/newdialogue.cpp
+++ src/mapeditor/newdialogue.cpp
@@ -2,9 +2,15 @@
 #include <exception>
 #include <stdexcept>
 
+#include "../engine/map/map.h"
 #include "newdialogue.h"
+#include "util.h"
 
 using namespace MapEditor;
+
+void mapEditorNewDialogueWrapperSizeWidthSpinButtonValuedChanged(GtkSpinButton *spinButton, gpointer userData);
+void mapEditorNewDialogueWrapperSizeHeightSpinButtonValuedChanged(GtkSpinButton *spinButton, gpointer userData);
+void mapEditorNewDialogueWrapperSizeUnitsComboChanged(GtkComboBox *combo, gpointer userData);
 
 namespace MapEditor {
 	NewDialogue::NewDialogue(GtkWidget *parentWindow) {
@@ -29,10 +35,15 @@ namespace MapEditor {
 		error|=(window=GTK_WIDGET(gtk_builder_get_object(builder, "window")))==NULL;
 		error|=(sizeWidthSpinButton=GTK_WIDGET(gtk_builder_get_object(builder, "sizeWidthSpinButton")))==NULL;
 		error|=(sizeHeightSpinButton=GTK_WIDGET(gtk_builder_get_object(builder, "sizeHeightSpinButton")))==NULL;
+		error|=(sizeUnitsCombo=GTK_WIDGET(gtk_builder_get_object(builder, "sizeUnitsCombo")))==NULL;
+		error|=(infoFileSizeLabel=GTK_WIDGET(gtk_builder_get_object(builder, "infoFileSizeLabel")))==NULL;
 		if (error)
 			throw std::runtime_error("could not grab main window widgets");
 
 		// Connect manual signals
+		g_signal_connect(sizeWidthSpinButton, "value-changed", G_CALLBACK(mapEditorNewDialogueWrapperSizeWidthSpinButtonValuedChanged), (void *)this);
+		g_signal_connect(sizeHeightSpinButton, "value-changed", G_CALLBACK(mapEditorNewDialogueWrapperSizeHeightSpinButtonValuedChanged), (void *)this);
+		g_signal_connect(sizeUnitsCombo, "changed", G_CALLBACK(mapEditorNewDialogueWrapperSizeUnitsComboChanged), (void *)this);
 
 		// Add OK and Cancel buttons
 		gtk_dialog_add_button(GTK_DIALOG(window), "Cancel", GTK_RESPONSE_CANCEL);
@@ -54,9 +65,12 @@ namespace MapEditor {
 	}
 
 	bool NewDialogue::run(unsigned *width, unsigned *height) {
-		// Update spin buttons based on given values
+		// Update interface based on given values
 		gtk_spin_button_set_value(GTK_SPIN_BUTTON(sizeWidthSpinButton), *width);
 		gtk_spin_button_set_value(GTK_SPIN_BUTTON(sizeHeightSpinButton), *height);
+		gtk_combo_box_set_active_id(GTK_COMBO_BOX(sizeUnitsCombo), "tiles");
+
+		updateInfo();
 
 		// Show the dialogue and wait for response
 		gtk_widget_show_all(window);
@@ -67,10 +81,78 @@ namespace MapEditor {
 			return false;
 
 		// Grab values to return
-		*width=gtk_spin_button_get_value(GTK_SPIN_BUTTON(sizeWidthSpinButton));
-		*height=gtk_spin_button_get_value(GTK_SPIN_BUTTON(sizeHeightSpinButton));
+		*width=getSizeWidth();
+		*height=getSizeHeight();
 
 		return true;
 	}
 
+	unsigned NewDialogue::getSizeWidth(void) {
+		unsigned width=gtk_spin_button_get_value(GTK_SPIN_BUTTON(sizeWidthSpinButton));
+		return width*getSizeUnitsFactor();
+	}
+
+	unsigned NewDialogue::getSizeHeight(void) {
+		unsigned height=gtk_spin_button_get_value(GTK_SPIN_BUTTON(sizeHeightSpinButton));
+		return height*getSizeUnitsFactor();
+	}
+
+	unsigned NewDialogue::getSizeUnitsFactor(void) {
+		if (strcmp(gtk_combo_box_get_active_id(GTK_COMBO_BOX(sizeUnitsCombo)), "regions")==0)
+			return 256;
+		if (strcmp(gtk_combo_box_get_active_id(GTK_COMBO_BOX(sizeUnitsCombo)), "km")==0)
+			return 1024;
+		return 1; // tiles
+	}
+
+	void NewDialogue::sizeWidthSpinButtonValuedChanged(GtkSpinButton *spinButton) {
+		updateInfo();
+	}
+
+	void NewDialogue::sizeHeightSpinButtonValuedChanged(GtkSpinButton *spinButton) {
+		updateInfo();
+	}
+
+	void NewDialogue::sizeUnitsComboChanged(GtkComboBox *combo) {
+		// Adjust limits for width/height fields based on units selected
+		unsigned factor=getSizeUnitsFactor();
+		double lower=(factor==1 ? 256.0 : 1.0);
+		double upper=65536.0/factor;
+		double stepInc=(factor==1 ? 256.0/factor : 1024.0/factor);
+		double pageInc=(factor==1 ? 1024.0/factor : 4096.0/factor);
+
+		GtkAdjustment *adjustment=gtk_spin_button_get_adjustment(GTK_SPIN_BUTTON(sizeWidthSpinButton));
+		double value=gtk_adjustment_get_value(adjustment);
+		gtk_adjustment_configure(adjustment, std::max(std::min(value, upper), lower), lower, upper, stepInc, pageInc, 0.0);
+		adjustment=gtk_spin_button_get_adjustment(GTK_SPIN_BUTTON(sizeHeightSpinButton));
+		value=gtk_adjustment_get_value(adjustment);
+		gtk_adjustment_configure(adjustment, std::max(std::min(value, upper), lower), lower, upper, stepInc, pageInc, 0.0);
+
+		// Update info
+		updateInfo();
+	}
+
+	void NewDialogue::updateInfo(void) {
+		// Update file size estimation
+		size_t fileSize=Engine::Map::Map::estimatedFileSize(getSizeWidth(), getSizeHeight());
+		char fileSizeStr[128]; // TODO: better
+		sizeToStr(fileSizeStr, fileSize);
+		gtk_label_set_text(GTK_LABEL(infoFileSizeLabel), fileSizeStr);
+	}
+
 };
+
+void mapEditorNewDialogueWrapperSizeWidthSpinButtonValuedChanged(GtkSpinButton *spinButton, gpointer userData) {
+	NewDialogue *newDialogue=(NewDialogue *)userData;
+	newDialogue->sizeWidthSpinButtonValuedChanged(spinButton);
+}
+
+void mapEditorNewDialogueWrapperSizeHeightSpinButtonValuedChanged(GtkSpinButton *spinButton, gpointer userData) {
+	NewDialogue *newDialogue=(NewDialogue *)userData;
+	newDialogue->sizeHeightSpinButtonValuedChanged(spinButton);
+}
+
+void mapEditorNewDialogueWrapperSizeUnitsComboChanged(GtkComboBox *combo, gpointer userData) {
+	NewDialogue *newDialogue=(NewDialogue *)userData;
+	newDialogue->sizeUnitsComboChanged(combo);
+}
--- src/mapeditor/newdialogue.h
+++ src/mapeditor/newdialogue.h
@@ -13,10 +13,22 @@ namespace MapEditor {
 		~NewDialogue();
 
 		bool run(unsigned *width, unsigned *height); // width and height specify initial values and also return output if dialogue is accepted
+
+		unsigned getSizeWidth(void); // in tiles
+		unsigned getSizeHeight(void); // in tiles
+		unsigned getSizeUnitsFactor(void); // 1 for tiles, 256 for regions, 1024 for km
+
+		void sizeWidthSpinButtonValuedChanged(GtkSpinButton *spinButton);
+		void sizeHeightSpinButtonValuedChanged(GtkSpinButton *spinButton);
+		void sizeUnitsComboChanged(GtkComboBox *combo);
 	private:
 		GtkWidget *window;
 		GtkWidget *sizeWidthSpinButton;
 		GtkWidget *sizeHeightSpinButton;
+		GtkWidget *sizeUnitsCombo;
+		GtkWidget *infoFileSizeLabel;
+
+		void updateInfo(void);
 	};
 
 };
