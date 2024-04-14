a3a3a18a3dae98ec4c7dd87e14900e530b7b0b2d
DJW <fake@example.com>
2024-04-14 12:36:04 +0100

    Made MapPngLib::getColourForTile public so editor can use it to render tiles as needed

--- src/common/emap/mappnglib.h
+++ src/common/emap/mappnglib.h
@@ -20,8 +20,10 @@ namespace Common {
 		class MapPngLib {
 		public:
 			static bool generatePng(class Map *map, const char *imagePath, int mapTileX, int mapTileY, int mapTileWidth, int mapTileHeight, int imageWidth, int imageHeight, MapTiled::ImageLayer layer, bool quiet);
-		private:
+
 			static void getColourForTile(class Map *map, int mapTileX, int mapTileY, const MapTile *tile, MapTiled::ImageLayer layer, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *a);
+
+		private:
 			static void getColourForTileHeight(class Map *map, int mapTileX, int mapTileY, const MapTile *tile, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *a);
 			static void getColourForTileTemperature(class Map *map, int mapTileX, int mapTileY, const MapTile *tile, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *a);
 			static void getColourForTileMoisture(class Map *map, int mapTileX, int mapTileY, const MapTile *tile, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *a);
